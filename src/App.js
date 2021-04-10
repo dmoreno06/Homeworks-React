d857cbb1f34453248f9c02ed56cfbccb03abd666
DMoreno06 <mordiana1506@gmail.com>
2021-04-09 21:36:39 -0500

    commit
    User interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="container mt-5">
       <h1 className="text-center">HOMEWORKS</h1>
-      <hr/>
+      <hr/> 
+        <div className="row">
+          <div className="col-8">
+            <h4 className="text-center">List of tasks</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <samp className="lead">Tasks name</samp>
+                <button className="btn badge-danger btn-sm float-right mx-2" type="">Delete</button>
+                <button className="btn badge-warning btn-sm float-right" type="">Edit</button>
+              </li>
+            </ul> 
+          </div>
+        <div className="col-4">
+          <h4 className="text-center">From</h4>
+          <form>
+            <input 
+            type="text" 
+            name="form-task"
+            className ="form-control mb-2"
+            value=""
+            placeholder="Ingrese la tarea.."/>
+            <button
+             type="submit" 
+             className="btn btn-dark btn-block"
+             >Agregar</button>
+          </form>
+        </div>
+    </div>    
     </div>
   );
 }
